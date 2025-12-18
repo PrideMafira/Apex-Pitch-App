ddb13b6a20b11a6e8048d655f56339880c46d69e
Pride Mafira <tadiwanashepride50@gmail.com>
2025-12-18 10:44:28 +0200

    created Home Page

---
+++ Apex-Pitch-App main/HomePage.swift
@@ -0,0 +1,91 @@
+//
+//  HomePage.swift
+//  Apex-Pitch-App main
+//
+//  Created by Pride Mafira  on 18/12/2025.
+//
+
+import SwiftUI
+
+struct HomePage: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Image("appIcon2")
+                    .resizable()
+                    .frame(width: 300,height: 300)
+                Spacer()
+      
+                NavigationLink {
+//                    Tabs()
+                } label: {
+                    Text("Add idea")
+                        .font(.title2)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(maxWidth: .infinity) // This makes the label expand horizontally
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                        .padding()
+                }
+                Spacer()
+//                .offset(x: 15)
+                
+                NavigationLink {
+//                    Notes_Page()
+                } label: {
+                    Text("Add notes")
+                        .font(.title2)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(maxWidth: .infinity) // This makes the label expand horizontally
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                        .padding()
+                }
+                Spacer()
+                
+                NavigationLink {
+//                    Zoom_Meetings_Page(showWebView: "https://zoom.us/signin")
+                } label: {
+                    Text("Meetings")
+                        .font(.title2)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(maxWidth: .infinity) // This makes the label expand horizontally
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                        .padding()
+                                    }
+                Spacer()
+                
+
+                        NavigationLink {
+//                            Settings_Page()
+                        } label: {
+                            Image(systemName: "gearshape.2")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 40, height: 40)
+                                .padding()
+                                .foregroundStyle(.black)
+                                .padding()
+                        }
+                        .offset(x: 150)
+                        Spacer()
+                    
+
+                
+            }
+            .navigationTitle("Apex Pitch")
+            .navigationSubtitle("Track your startup ideas.")
+            
+            
+            
+        }
+    }
+}
+
+#Preview {
+    HomePage()
+}
